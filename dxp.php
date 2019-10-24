381fb2006d4b71def2d9a0227e4be16baacd5558
Alexandru Nitu <alexn@crowdfavorite.com>
2019-10-24 04:02:25 +0300

    initial commit

---
+++ dxp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File holding logic related to the DXP Dashboard.
+ *
+ * @package cf-dxpct
+ */
+
+// Hook Custom Updater
+new OSDXP_Dashboard\OsdxpModuleUpdateChecker(
+	OSDXP_LL_UPDATE_URL,
+	OSDXP_LL_PLUGIN_FILE,
+	OSDXP_LL_PLUGIN_SLUG
+);
+
+// Change button text
+add_filter('osdxp_manage_button_module_osdxp-login-logo', function () {
+	return 'View Website';
+});
